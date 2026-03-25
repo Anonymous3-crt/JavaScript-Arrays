a613bc4c8312fc94c42cd683cf23fa9226f3649b
charles senator solomon <senatorsolomon735@gmail.com>
2026-03-25 22:39:49 +0100

    Question15

--- Question15.js
+++ Question15.js
@@ -1,14 +1,14 @@
-// 1. Create the array
-let nums = [3, 7, 2, 9, 5];
+let marks = [40, 55, 72, 88, 63];
+let count = 0;
 
-// 2. Create a variable to hold the total, starting at 0
-let total = 0;
-
-// 3. Use a for loop to add each number to the total
-for (let i = 0; i < nums.length; i++) {
-  total = total + nums[i]; 
-  
+for (let i = 0; i < marks.length; i++) {
+  if (marks[i] > 60) {
+    count++; 
+    console.log(`Checking ${marks[i]}... Yes! Count is now ${count}`);
+  } else {
+    console.log(`Checking ${marks[i]}... No.`);
+  }
 }
 
-
-console.log("The total sum is: " + total);
+console.log("---------------------------");
+console.log("FINAL COUNT:", count);
